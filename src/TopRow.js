4727fd36164306dbef56edc645960361f4776c9a
Kevin Tou <toukevin@gmail.com>
2019-07-16 09:18:02 -0700

    Updated timer functionality

--- src/TopRow.js
+++ src/TopRow.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const TopRow = props => {
+  const [minutes, setMinutes] = useState(15);
+  const [seconds, setSeconds] = useState("00");
+
+  let timer = 900;
+
+  useEffect(() => {
+    let interval = null;
+
+    interval = setInterval(() => {
+      setMinutes(
+        Math.floor(timer / 60) < 10
+          ? String(Math.floor(timer / 60)).padStart(2, 0)
+          : Math.floor(timer / 60)
+      );
+      setSeconds(
+        Math.floor(timer % 60) < 10
+          ? String(Math.floor(timer % 60)).padStart(2, 0)
+          : Math.floor(timer % 60)
+      );
+      timer--;
+      if (timer < 0) {
+        clearInterval(interval);
+      }
+    }, 1000);
+  }, [timer]);
+
   return (
     <div className="topRow">
       <div className="home">
@@ -10,7 +36,9 @@ const TopRow = props => {
 
         <div className="home__score">{props.homeScore}</div>
       </div>
-      <div className="timer">00:03</div>
+      <div className="timer">
+        {minutes}:{seconds}
+      </div>
       <div className="away">
         <h2 className="away__name">{props.away}</h2>
         <div className="away__score">{props.awayScore}</div>
